004e790e3b925e49ef2ceef86ae5e595cc695835
guymn <117151853+guymn@users.noreply.github.com>
2023-10-26 16:04:02 +0700

    touter-outlat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,32 +13,39 @@ import { ProfileComponent } from './profile/profile.component';
 import { ProfileGameComponent } from './profile-game/profile-game.component';
 import { ProfileTeamComponent } from './profile-team/profile-team.component';
 import { ProfileProfileComponent } from './profile-profile/profile-profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  {
+    path: '',
+    component: NavbarComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'leauges', component: LeaugesComponent },
+      { path: 'scrims', component: ScrimsComponent },
+      { path: 'tournament', component: TournamentComponent },
+      {
+        path: 'finder',
+        component: FinderComponent,
+        children: [
+          { path: '', redirectTo: 'player', pathMatch: 'full' }, // Redirect empty path to 'player'
+          { path: 'player', component: FinderPlayerComponent },
+          { path: 'team', component: FinderTeamComponent },
+        ],
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+        children: [
+          { path: 'profile', component: ProfileProfileComponent },
+          { path: 'game', component: ProfileGameComponent },
+          { path: 'team', component: ProfileTeamComponent },
+        ],
+      },
+    ],
+  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'leauges', component: LeaugesComponent },
-  { path: 'scrims', component: ScrimsComponent },
-  { path: 'tournament', component: TournamentComponent },
-  {
-    path: 'finder',
-    component: FinderComponent,
-    children: [
-      { path: '', redirectTo: 'player', pathMatch: 'full' }, // Redirect empty path to 'player'
-      { path: 'player', component: FinderPlayerComponent },
-      { path: 'team', component: FinderTeamComponent },
-    ],
-  },
-  {
-    path: 'profile',
-    component: ProfileComponent,
-    children: [
-      { path: 'profile', component: ProfileProfileComponent },
-      { path: 'game', component: ProfileGameComponent },
-      { path: 'team', component: ProfileTeamComponent },
-    ],
-  },
 ];
 
 @NgModule({
