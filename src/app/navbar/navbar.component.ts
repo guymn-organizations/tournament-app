6cbde2bfcff39f7dd85bc403b3027b5fb692c969
guymn <117151853+guymn@users.noreply.github.com>
2023-10-27 17:53:36 +0700

    pointer

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -12,10 +12,10 @@ export class NavbarComponent {
   menu = false;
   navBarName = ['leauges', 'scrims', 'tournament', 'finder'];
   navBarImg = [
-    'https://cdn.discordapp.com/attachments/1163138471196622918/1163140864357437570/370257351_657778326338320_3000149127718713003_n.png?ex=653e7e58&is=652c0958&hm=85b1d127e95ed383906c426ffc1b965388762b7319700229f4b62ec5f4826c21&',
     '../../assets/img/nav/SCRIMS.png',
-    'https://cdn.discordapp.com/attachments/1163138471196622918/1163140863652794388/387344873_730245708930192_4520940304685637453_n.png?ex=653e7e58&is=652c0958&hm=456aceb3e92d9199bc83cd79393efc0a01d9ef9a597fa0b385041be350413d91&',
-    'https://cdn.discordapp.com/attachments/1163138471196622918/1163140864139350148/387593649_2840731006069136_1036389285473139054_n.png?ex=653e7e58&is=652c0958&hm=260356779da873b2e5a45570cf89747f1c8935a2fc9e02316c5bad33096ed024&',
+    '../../assets/img/nav/LEAUGES.png',
+    '../../assets/img/nav/TOURNAMENT.png',
+    '../../assets/img/nav/FINDER.png',
   ];
 
   constructor() {}
@@ -32,9 +32,8 @@ export class NavbarComponent {
   checkTab() {
     return true;
   }
-  
+
   toProfile(page: string) {
     this.service.toPage(page);
   }
-
 }
