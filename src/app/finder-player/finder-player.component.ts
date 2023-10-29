30ddd5dc03c8b33652e44cc15c4804d3c81c8a7d
Ezyzz <sktballkung02@gmail.com>
2023-10-29 21:06:55 +0700

    finder waiting database

--- src/app/finder-player/finder-player.component.ts
+++ src/app/finder-player/finder-player.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-finder-player',
   templateUrl: './finder-player.component.html',
   styleUrls: ['./finder-player.component.css']
 })
-export class FinderPlayerComponent {
+export class FinderPlayerComponent implements OnInit {
+
+  Name : String = '';
+  users = [
+    {
+      image : "",
+      name: 'DSL',
+      poolhero: '75+'
+    },
+    {
+      image : "",
+      name: 'JG',
+      poolhero: '75+'
+    },
+    {
+      image : "",
+      name: 'MID',
+      poolhero: '75+'
+    },
+    {
+      image : "",
+      name: 'SUP',
+      poolhero: '75+'
+    },
+
+
+  ]
+
+  ngOnInit(): void {
+   
+  }
+
 
 }
--- src/app/finder-team/finder-team.component.ts
+++ src/app/finder-team/finder-team.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-finder-team',
   templateUrl: './finder-team.component.html',
   styleUrls: ['./finder-team.component.css']
 })
-export class FinderTeamComponent {
+export class FinderTeamComponent implements OnInit{
 
+    Name : String = '';
+    users = [
+      {
+        name: 'DSL',
+        poolhero: '75+'
+      },
+      {
+        name: 'JG',
+        poolhero: '75+'
+      },
+      {
+        name: 'MID',
+        poolhero: '75+'
+      },
+      {
+        name: 'SUP',
+        poolhero: '75+'
+      },
+  
+  
+    ]
+  
+    ngOnInit(): void {
+     
+    }
 }
