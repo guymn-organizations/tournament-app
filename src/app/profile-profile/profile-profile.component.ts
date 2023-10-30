e4dac254b5e9f8a4326d58f033068e700b444a39
guymn <117151853+guymn@users.noreply.github.com>
2023-10-30 14:52:57 +0700

    out image

--- src/app/profile-profile/profile-profile.component.ts
+++ src/app/profile-profile/profile-profile.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { Profile } from '../model/profile';
-import { GobalServiceService } from '../service/gobal-service.service';
 
 @Component({
   selector: 'app-profile-profile',
@@ -9,7 +8,6 @@ import { GobalServiceService } from '../service/gobal-service.service';
   styleUrls: ['./profile-profile.component.css'],
 })
 export class ProfileProfileComponent {
-  selectedImageURL: string | ArrayBuffer | null = null;
   nav: NavbarComponent = inject(NavbarComponent);
 
   constructor() {}
@@ -20,29 +18,5 @@ export class ProfileProfileComponent {
     }
 
     return new Profile();
-  }
-
-  onFileSelected(event: any) {
-    const file: File = event.target.files[0];
-
-    if (file) {
-      this.saveImage(file);
-    }
-  }
-
-  saveImage(file: File) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      if (e.target) {
-        this.selectedImageURL = e.target.result;
-        this.uploadImage(this.selectedImageURL as string);
-      }
-    };
-    reader.readAsDataURL(file);
-  }
-
-  async uploadImage(image: string) {
-    this.nav.getProfile().imageProfileUrl = image;
-    await this.nav.updateProfile();
   }
 }
