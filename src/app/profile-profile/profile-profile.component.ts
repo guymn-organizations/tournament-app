79aeda4541713e65cb3b47384eac4c459a3e45f9
guymn <117151853+guymn@users.noreply.github.com>
2023-10-31 23:09:00 +0700

    edit profile

--- src/app/profile-profile/profile-profile.component.ts
+++ src/app/profile-profile/profile-profile.component.ts
@@ -10,5 +10,51 @@ import { Profile } from '../model/profile';
 export class ProfileProfileComponent {
   nav: NavbarComponent = inject(NavbarComponent);
 
+  profileData = {
+    first_name: this.nav.profile?.firstName,
+    last_name: this.nav.profile?.lastName,
+    email: this.nav.profile?.email,
+    password: '',
+    confirm_password: '',
+    birthday: this.nav.profile?.birthday,
+  };
+
+  toEdit = false;
+  errorMessage: string = '';
+
   constructor() {}
+
+  async setprofileData() {
+    this.profileData.first_name = this.nav.profile?.firstName;
+    this.profileData.last_name = this.nav.profile?.lastName;
+    this.profileData.email = this.nav.profile?.email;
+    this.profileData.birthday = this.nav.profile?.birthday;
+  }
+  isEdit(): boolean {
+    return !!this.toEdit;
+  }
+
+  clickEdit() {
+    this.setprofileData();
+    this.toEdit = true;
+  }
+
+  clickOutEdit() {
+    this.toEdit = false;
+  }
+
+  onSubmitEditForm() {
+    if (this.profileData.password !== this.profileData.confirm_password) {
+      this.errorMessage = 'Password and confirm do not math';
+      return;
+    }
+    this.clickOutEdit();
+
+    this.nav.getProfile().birthday = this.profileData.birthday as Date;
+    this.nav.getProfile().firstName = this.profileData.first_name as string;
+    this.nav.getProfile().lastName = this.profileData.last_name as string;
+    this.nav.getProfile().email = this.profileData.email as string;
+    this.nav.getProfile().password = this.profileData.password as string;
+    this.nav.updateProfile();
+  }
 }
